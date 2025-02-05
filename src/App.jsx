f6e9db37e7965df74e37fb15eb6f5f3b305b8e67
camille andrade <camille.andrade@logan.br>
2025-02-05 14:04:43 -0300

    tela de Welcome, falta arrumar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { GoAlert } from "react-icons/go";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Button } from "./components/button";
+import { useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
+import {Welcome} from "./pages/Welcome/index.jsx"
+
 
 export function App() {
   return <>
     <h1>Operação Reciclagem</h1>
     <Button rounded={true}><IoIosArrowBack /></Button>
     <Button>Play</Button>
+    <Welcome/>
+
   </>
 }
+
+
+
+
+
+
